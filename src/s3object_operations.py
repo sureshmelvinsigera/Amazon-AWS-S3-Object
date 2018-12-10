190963f923ef42405184f3a6656dcfefbb9fd37e
Suresh Melvin Sigera <sureshmelvinsigera@users.noreply.github.com>
2018-12-10 16:35:49 -0500

    Added create_bucket_policy()

--- src/s3object_operations.py
+++ src/s3object_operations.py
@@ -1,5 +1,6 @@
 import boto3
 from botocore.client import ClientError
+import json
 
 
 def s3_client():
@@ -10,11 +11,26 @@ def s3_client():
 
 def create_bucket(name):
     try:
-        return s3_client().create_bucket(Bucket=name, CreateBucketConfiguration={
-            'LocationConstraint': 'us-west-1'})
+        s3_client().create_bucket(Bucket=name, CreateBucketConfiguration={'LocationConstraint': 'us-west-1'})
+        create_bucket_policy(name)
     except ClientError:
         print("The bucket does not exist or you have no access.")
 
 
+def create_bucket_policy(name):
+    bucket_policy = {
+        'Version': '2012-10-17',
+        'Statement': [{
+            'Sid': 'AddPerm',
+            'Effect': 'Allow',
+            'Principal': '*',  # allow to public
+            'Action': ['s3:*'],  # allow all the operations
+            'Resource': ["arn:aws:s3:::%s/*" % name]
+        }]
+    }
+
+    policy_string = json.dumps(bucket_policy)
+
+
 if __name__ == '__main__':
-    create_bucket("gmp232321")
+    create_bucket("gmp23232111")
